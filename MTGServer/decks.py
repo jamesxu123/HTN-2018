53bc3257259284991f2af2a57c51e11285592c03
James Xu <xujames007@gmail.com>
2018-09-16 03:11:47 -0400

    for noor's sake

--- MTGServer/decks.py
+++ MTGServer/decks.py
@@ -11,7 +11,7 @@ class DeckHandler(auth.AuthObject):
             cur.execute("SELECT deck FROM users WHERE username = '%s';" % username)
             for row in cur.fetchall():
                 deck = row[0]
-                return json.dumps(deck)
+                return json.loads(deck)
         return False
 
     def set_deck(self, username, new_deck_name, new_deck):
